67d822aced5f310c59b39d3ded053e757110ce53
Dmytro Kononenko <dmytro.kononenko@hotmail.com>
2014-10-31 21:18:18 +0200

    Empty Feed page added

---
+++ TravelMapNewVersion/Controllers/FeedController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using DotNetOpenAuth.Messaging;
+using nonintanon.Security;
+using TravelMap.Models;
+
+namespace TravelMap.Controllers
+{
+	 [Authorize]
+    public class FeedController : Controller
+    {
+		 private Entities db = new Entities();
+
+        // GET: Feed
+        public ActionResult Index()
+        {
+			var userId = WebSecurity.CurrentUserId;
+			return View("~/Views/Feed.cshtml", userId);
+        }
+
+		public JsonResult JIndex(Guid id)
+		{
+			var userProfile = db.UserProfiles.Find(id);
+			if (userProfile == null)
+			{
+				return new JsonResult();
+			}
+			var posts = userProfile.Posts;
+			var fposts = userProfile.Followers.Select(f => f.UserProfile.Posts);
+			foreach (var fpost in fposts)
+			{
+				posts.AddRange(fpost);
+			}
+
+			// todo: create class to send all kinds of news: reports, new followers etc.
+			posts = posts.OrderByDescending(p => p.Time).ToList();
+			
+			var result = Json(posts, JsonRequestBehavior.AllowGet);
+			// todo: add friends reports to result
+			
+			return result;
+		}
+    }
+}
