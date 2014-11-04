5e47774e22666565e68955f5f2d7c1560816d2db
Alexander Belenko <AlexBelenko@outlook.com>
2014-11-04 12:21:05 +0200

    Change design of popUp, travels and reports.

--- TravelMapNewVersion/Scripts/Map/popUp.js
+++ TravelMapNewVersion/Scripts/Map/popUp.js
@@ -9,10 +9,11 @@
             //console.log($scope.currentCountry);
             $scope.$apply();
         }
-        var travel1 = { 'start': '2014-10-13', 'end': '2014-11-13', 'message': "You can terminate your session by clicking the log out button in the top right corner. This prevents unauthorized use of your mail.com mailbox, for example, by clicking on the back button of your browser. Please remember to always click on the log out button for your own safety when exiting your mailbox." },
-            travel2 = { 'start': '2014-2-13', 'end': '2014-2-155', 'message': "You can terminate your session by clicking the log out button in the top right corner. This prevents unauthorized use of your mail.com mailbox, for example, by clicking on the back button of your browser. Please remember to always click on the log out button for your own safety when exiting your mailbox." };
-        $scope.travels = [travel1,travel2];
-      
+        //var travel1 = { 'start': '2014-10-13', 'end': '2014-11-13', 'message': "You can terminate your session by clicking the log out button in the top right corner. This prevents unauthorized use of your mail.com mailbox, for example, by clicking on the back button of your browser. Please remember to always click on the log out button for your own safety when exiting your mailbox." },
+        //    travel2 = { 'start': '2014-2-13', 'end': '2014-2-155', 'message': "You can terminate your session by clicking the log out button in the top right corner. This prevents unauthorized use of your mail.com mailbox, for example, by clicking on the back button of your browser. Please remember to always click on the log out button for your own safety when exiting your mailbox." };
+        //$scope.travels = [travel1, travel2];
+        $scope.travels = [];
+
 
     });
 
@@ -22,14 +23,12 @@
         this.travel = {};
 
         this.clickCancel = function () {
-            console.log(this.travel);
             $scope.$parent.currentCountry = null;
             this.travel = {};
         };
 
 
-
-        this.clickOK = function () {
+        this.clickOK = function() {
             var trvl = this.travel;
             //this.travel.start = this.travel.start.split("-");
             //this.travel.end = this.travel.end.split("-");
@@ -37,21 +36,21 @@
                 country: $scope.$parent.currentCountry,
                 start: new Date(this.travel.start).getTime() / 1000,
                 end: new Date(this.travel.end).getTime() / 1000,
-            }).success(function (data, status, headers, config) {
-                console.log(data);
-                console.log(trvl.message);
+            }).success(function(data, status, headers, config) {
 
                 $http.post('/Post/PostReport', {
                     text: trvl.message,
                     travelId: data
                 });
-                    console.log('success');
-                }).error(function (data, status, headers, config) {
-                    console.log('error');
-                });
-            this.travel = {};
-            $scope.$parent.currentCountry = null;
-        }
+                trvl = {};
+                $scope.$parent.currentCountry = null;
+            }).error(function(data, status, headers, config) {
+                //  console.log('error');
+                alert(status);
+            });
+            //this.travel = {};
+            //$scope.$parent.currentCountry = null;
+        };
     });
 
 })();
