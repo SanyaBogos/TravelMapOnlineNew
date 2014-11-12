7c2a1aacb2a69e85b11adabf6560be7d903c292b
sanches <bog_alex_ur@ukr.net>
2014-11-12 11:46:11 +0200

    Database (and model) changes that need for chat.

---
+++ TravelMapNewVersion/Hubs/ChatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using nonintanon.Security;
+using TravelMap.Models;
+
+namespace TravelMap.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private Entities db = new Entities();
+
+        public void Send(string name, string message)
+        {
+            //var currentUser = WebSecurity.CurrentUserId;
+            //string name = db.UserProfiles.First(u => u.UserId == currentUser).FirstName;
+            // Call the addNewMessageToPage method to update clients. 
+            Clients.All.addNewMessageToPage(name, message);
+        }
+    }
+}
